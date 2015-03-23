78ac4aa176d4ffaf3b8638ee81a5c975e8af0443
Carlos <carlosniper1@gmail.com>
2015-03-23 20:19:02 -0300

    mudei o home

--- js/iluminacao.js
+++ js/iluminacao.js
@@ -1,7 +1,24 @@
-function ligar(){
+function ligar1(){
 	document.getElementById("status").src="img/on.png";
 }
 
-function desligar(){
+function desligar1(){
 	document.getElementById("status").src="img/off.png";
 }
+
+function ligar2(){
+	document.getElementById("statusq").src="img/on.png";
+}
+
+function desligar2(){
+	document.getElementById("statusq").src="img/off.png";
+}
+
+function ligar3(){
+	document.getElementById("statusb").src="img/on.png";
+}
+
+function desligar3(){
+	document.getElementById("statusb").src="img/off.png";
+}
+
