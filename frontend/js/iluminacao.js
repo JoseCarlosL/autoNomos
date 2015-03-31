8760da8c6798c9880f0895d459013a45fa929d0d
Haysa <rodrigues.haysa@gmail.com>
2015-03-31 19:58:42 -0300

    Organização

---
+++ frontend/js/iluminacao.js
@@ -0,0 +1,73 @@
+function ligarSala1(){
+	document.getElementById("status1").src="img/on.png";
+}
+
+function desligarSala1(){
+	document.getElementById("status1").src="img/off.png";
+}
+
+function ligarSala2(){
+	document.getElementById("status2").src="img/on.png";
+}
+
+function desligarSala2(){
+	document.getElementById("status2").src="img/off.png";
+}
+
+function ligarSala3(){
+	document.getElementById("status3").src="img/on.png";
+}
+
+function desligarSala3(){
+	document.getElementById("status3").src="img/off.png";
+}
+//Quarto
+function ligarQuarto1(){
+	document.getElementById("statusq1").src="img/on.png";
+}
+
+function desligarQuarto1(){
+	document.getElementById("statusq1").src="img/off.png";
+}
+
+function ligarQuarto2(){
+	document.getElementById("statusq2").src="img/on.png";
+}
+
+function desligarQuarto2(){
+	document.getElementById("statusq2").src="img/off.png";
+}
+
+function ligarQuarto3(){
+	document.getElementById("statusq3").src="img/on.png";
+}
+
+function desligarQuarto3(){
+	document.getElementById("statusq3").src="img/off.png";
+}
+
+//banheiro
+function ligarBanheiro1(){
+	document.getElementById("statusb1").src="img/on.png";
+}
+
+function desligarBanheiro1(){
+	document.getElementById("statusb1").src="img/off.png";
+}
+
+function ligarBanheiro2(){
+	document.getElementById("statusb2").src="img/on.png";
+}
+
+function desligarBanheiro2(){
+	document.getElementById("statusb2").src="img/off.png";
+}
+
+function ligarBanheiro3(){
+	document.getElementById("statusb3").src="img/on.png";
+}
+
+function desligarBanheiro3(){
+	document.getElementById("statusb3").src="img/off.png";
+}
+
---
+++ frontend/js/platClient.js
@@ -0,0 +1,3 @@
+function load_home(){
+	document.getElementById("content").innerHTML='<object type="text/html" data="cont.html"><iframe src="cont.html"></iframe></object>';
+}
