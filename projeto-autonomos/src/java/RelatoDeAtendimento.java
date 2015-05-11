5f774cc24806655ef307b162cd8225f152f75a4f
Haysa <rodrigues.haysa@gmail.com>
2015-05-11 20:16:18 -0300

    1º servlet

--- projeto-autonomos/src/java/RelatoDeAtendimento.java
+++ projeto-autonomos/src/java/RelatoDeAtendimento.java
@@ -5,18 +5,30 @@ import java.util.Date;
 public class RelatoDeAtendimento {
 
 	private int id;
+	private String nome;
+
+	private String email;
+	private String telefone;
 	private String data;
 	private int id_visitante;
 	private boolean atendido;
 	private String duvida;
 
-	public RelatoDeAtendimento(int id, String data, int id_visitante, boolean atendido,
+	public RelatoDeAtendimento(int id, String nome, String email,
+			String telefone, String data, int id_visitante, boolean atendido,
 			String duvida) {
 		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
 		this.data = data;
 		this.id_visitante = id_visitante;
 		this.atendido = atendido;
 		this.duvida = duvida;
+	}
+	
+	public RelatoDeAtendimento(){
+		
 	}
 
 	protected int getId() {
@@ -27,11 +39,35 @@ public class RelatoDeAtendimento {
 		this.id = id;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	protected String getData() {
 		return data;
 	}
 
-	protected void setData(String data) {
+	public void setData(String data) {
 		this.data = data;
 	}
 
@@ -55,7 +91,7 @@ public class RelatoDeAtendimento {
 		return duvida;
 	}
 
-	protected void setDuvida(String duvida) {
+	public void setDuvida(String duvida) {
 		this.duvida = duvida;
 	}
 
---
+++ projeto-autonomos/src/servlets/ServletNovoFormularioContato.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.RelatoDeAtendimento;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/contato")
+public class ServletNovoFormularioContato extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// pegando os parametros da página
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String telefone = request.getParameter("telefone");
+		String dataEmTexto = request.getParameter("data");
+		Calendar data = null;
+		try {
+			Date d = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			data = Calendar.getInstance();
+			data.setTime(d);
+		} catch (ParseException e) {
+
+			System.out.println("Erro na conversão da data.");
+		}
+		String duvida = request.getParameter("duvida");
+		PrintWriter r = response.getWriter();
+		r.println("Em breve entraremos em contato com você!");
+
+		System.out.println(nome + email + telefone + dataEmTexto + duvida);
+
+	}
+
+}
