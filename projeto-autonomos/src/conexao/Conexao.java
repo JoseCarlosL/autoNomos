8f09fdd4036bb6b286703ded229730685d27ea5d
JoseCarlosL <carlosniper1@gmail.com>
2015-05-12 20:00:11 -0300

    testes com banco

---
+++ projeto-autonomos/src/conexao/Conexao.java
@@ -0,0 +1,44 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
+public class Conexao {
+	
+	public static String status = "";
+	static String url = "jdbc:mysql://localhost/testhome?user=root&password=carlos";
+	
+	public static Connection getConnection(){
+		Connection conn= null;
+		try{
+			
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(url);
+			java.sql.PreparedStatement stm = conn.prepareStatement("select * from ecohome");
+			ResultSet rs = stm.executeQuery();
+			while(rs.next()){
+				status = rs.getString("valor");
+				System.out.println(status);
+			}
+			
+			status = "Conexao aberta";
+		}catch(SQLException e){
+			status = e.getMessage();
+		}catch (ClassNotFoundException e) {
+			status = e.getMessage();
+		}catch (Exception e) {
+			status = e.getMessage();
+		}
+		return conn;
+		
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		Conexao.getConnection();
+		System.out.println(Conexao.status);
+	}
+}
